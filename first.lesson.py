f8dcf307bc8f862eadecf4c7a74779d0686ea24e
Aliya <kachibekova.06@gmail.com>
2025-10-23 20:33:11 +0600

    now i am adding 2 files and learning python and git hub as well

---
+++ first.lesson.py
@@ -0,0 +1,46 @@
+# question1 = input("What is your name?")
+# print("Her name is", question1)
+# question2 = input("How old are you?")
+# print("She is", question2, "years old")
+# print("Nice to meet you!")
+# question3 = input("Try to guess how old am i?")
+# print("Actually i am 19 years old!")
+# question4 = input("what are you interested in?")
+# print("Great!", question4, "i would like to try too!")
+
+
+# question5 = input("How much is this apples per kg")
+# print("That is too expensive!")
+# input("well for you it will cost only:")
+# print("Thanks! i will get 3 kg then")
+
+# int
+# float
+# str
+
+# num1 = 5
+# num1 = 5 + 1
+# num1 = num1 + 1
+# num1 += 1
+# print(num1)
+
+# # += 
+# # -+
+# # *=
+# # /= 
+
+# num2 = 2.3
+# print(type(num2))
+# print(type(num1))
+
+num1 = "5"
+num2 = 10
+num3 = 6.5
+dif = num2 - num3 
+print(dif)
+dif2 = num2 * num1
+print(dif2)
+
+ageIsAllowed = True 
+print(5<10)
+print(11>12)
