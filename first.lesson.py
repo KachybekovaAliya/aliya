0b433865a518ed212a655864994faa637f12a644
Aliya <kachibekova.06@gmail.com>
2025-10-24 21:07:46 +0600

    friday commit

--- first.lesson.py
+++ first.lesson.py
@@ -34,12 +34,12 @@
 # print(type(num1))
 
 num1 = "5"
-num2 = 10
-num3 = 6.5
-dif = num2 - num3 
-print(dif)
-dif2 = num2 * num1
-print(dif2)
+# num2 = 10
+# num3 = 6.5
+# dif = num2 - num3 
+# print(dif)
+# dif2 = num2 * num1
+# print(dif2)
 
 ageIsAllowed = True 
 print(5<10)
---
+++ thirdLesson.py
@@ -0,0 +1,93 @@
+# print("""Hello?
+#       i am tralla
+#       here for tatatat
+#       lalala""") 
+# # We can use triple " " for big texts 
+# print("Hello 'Sam'")
+# # we also can use '' inside ""
+# products = ['milk', 'bread', 'eggs']
+
+# print(products)
+
+# print(products[1]) we use [] to choose something from our list in this case we start counting from 0
+
+# product = list()
+
+
+# print("Hello, what do you want?")
+# products2 = ["milk", 5, "eggs", 5<10, 9.5]
+
+# print("Hello, i need:", products2)
+# print(products2[0], "How much does it cost")
+
+# price = int(input("Prise is:"))
+# input(price)
+
+# print(products2[2], "How much does it cost")
+
+# price2 = int(input("Prise is:"))
+# input(price2)
+
+# print("Ok here 500 som. Is it enough?")
+# print("let me check!")
+
+# total = price + price2
+
+# print("Change is:", 500-int(total))
+
+# print('hello', 344, 99.99, sep = '\n', end = '*')
+# sep = ' ' separating each word with anything you add in '' (if you add there '\n' it will sepatare each word by locating it on next line  )
+# end = ' '  ending sentence with anything you add in ''
+
+# num = 5 / 2         #   / division (деление)
+# print(num)
+# num2 = 5 // 2       #   //  division without ending (деление без остатка)
+# print(num2)
+# num3 = 101 % 10     # % to find remainder from devision (остаток от деления)
+# print(num3)
+# num4 = 3 * 2 
+# print(num4)
+# num5 = 3 ** 2
+# print(num5)
+
+
+# Task 1
+
+print(2*(3-1))
+print((5-1)*(5+1))
+print(0.3*(4-1))
+print((991-1)/(2+1))
+print(5*4/3 ** .5)
+print(1/3+5*(0.2-0.001)/(91))
+print((40-(4.25*7.08+6.768/0.75))*2050)
+print((0.16*(3.2-3/40)+2*3/11*4.125/3*3/4)/(5*1/6*0.3-0.3*4.5+1/3*0.3))
+
+# Task 2
+x = 24
+y = 31.4
+print(type(x))
+print(type(y))
+
+# Task 3
+a = 14
+b = 10
+dif = a - b 
+print(dif)
+
+#  Task 4
+potatos = 290
+bag = 25
+full = potatos / bag 
+print("Amount of bags will be full:", float(full))
+left = (full - 11) * 25
+print("Kg will be left", float(left))
+
+# Task 5
+h1 = 13
+m1 = 25
+h2 = 19
+m2 = 40
+h = (h1 - h2)
+m = (m1 - m2)
+print(int(h))
+print(int(m))
